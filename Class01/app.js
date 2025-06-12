018bf44c25b3a825c04508d4a950df7204d5e80e
farhan-20574 <farhan20574@outlook.com>
2025-06-12 10:39:21 +0500

    middle of class

---
+++ Class01/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('INSIDE MIDDLEWARE');
+    next();
+});
+
+app.get('/users', (req, res) => {
+    console.log('INSIDE ROUTE');
+    res.send('GET request received');
+});
+
+app.get('/users/:username', (req, res) => {
+    console.log(req.params.username);
+    res.send('GET request received');
+});
+
+app.post('/', (req, res) => {
+    console.log('INSIDE POST ROUTE');
+    res.send('POST request received');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000');
+});
