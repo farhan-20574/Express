22cb41dbe36a6dfcf3b81f9cacfa504bf146d99d
farhan-20574 <farhan20574@outlook.com>
2025-06-16 20:00:27 +0500

    home session 1st

--- Class02/app_FSmodule copy.js
+++ Class02/app_FSmodule copy.js
@@ -33,41 +33,41 @@ app.use((req , res, next)=>{
 
 
 
-// // app.get('/users/{:id}',(req,res)=>{
-// //     if (!req.params.id) return res.send(users)
+app.get('/users/{:id}',(req,res)=>{
+    if (!req.params.id) return res.send(users)
     
     
-// //         const user = users.find((user)=> user.id === parseInt(req.params.id))
-// //         if (!user) return res.status(404).send({message: "User ot found"})
-// //         res.send(user)
-// // //     });
+        const user = users.find((user)=> user.id === parseInt(req.params.id))
+        if (!user) return res.status(404).send({message: "User ot found"})
+        res.send(user)
+//     });
 
-// // app.get('/users/:id',(req,res)=>{
-// //     if (!req.params.id) {
-// //         return new Error("ID required")
-// //     }
+app.get('/users/:id',(req,res)=>{
+    if (!req.params.id) {
+        return new Error("ID required")
+    }
     
-// //         const user = users.find((user)=> user.id === parseInt(req.params.id))
-// //         if (!user) return res.status(404).send({message: "User ot found"})
-// //         res.send(user)
-// //     });
+        const user = users.find((user)=> user.id === parseInt(req.params.id))
+        if (!user) return res.status(404).send({message: "User ot found"})
+        res.send(user)
+    });
 
-// app.post('/users', (req,res)=>{
+app.post('/users', (req,res)=>{
 
-//     // console.log('>>>',req.body) //for see body
-//     const user ={
-//         id: Date.now(),
-//         first_name: req.body.first_name,
-//         last_name: req.body.last_name,
-//         email: req.body.email,
-//         age: req.body.age,
-//         gender:req.body.gender
-//     }
-//     users.push(user)
-//     res.status(201).send({message: 'user created sucessfully'})
-// })
+    // console.log('>>>',req.body) //for see body
+    const user ={
+        id: Date.now(),
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        age: req.body.age,
+        gender:req.body.gender
+    }
+    users.push(user)
+    res.status(201).send({message: 'user created sucessfully'})
+})
 
 
-// app.listen(3000, ()=>{
-//     console.log('server is running on po.rt 3000')
-// })
+app.listen(3000, ()=>{
+    console.log('server is running on po.rt 3000')
+})
