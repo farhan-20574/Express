c6f3b3285bb03031fe2ece80083d95720738f3b3
farhan-20574 <farhan20574@outlook.com>
2025-07-08 23:34:37 +0500

    refactor: Clean up app.js structure and comments for better readability

--- project9725/src/app.js
+++ project9725/src/app.js
@@ -1,27 +1,24 @@
 
-
-//student-portal
-
-//src Entry point of the application
-
-
-//controllers          # Contains controller files
-    // index.js        # Index controller for handling routes
-
-
-//routes               # Contains route definitions
-    //routes.js        # Route definitions for the application
-
+//Entry point of the application
+const express = require('express');
+const app = express();
 
 //middleware           # Contains middleware functions
-    //auth.js         # Authentication middleware
+    //auth.js          # Authentication middleware
 
 //views                # Contains EJS view templates
-    //index.ejs       # Homepage template
+    //index.ejs        # Homepage template
 
+
+ //controllers          # Contains controller files
+    // index.js         # Index controller for handling routes
+    //routes.js         # Route definitions for the application
 
 //utils                # Utility functions
-    //jwt.js          # JWT utility functions
+    //jwt.js            # JWT utility functions
 
-
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`server started @ port${PORT}`)
+})
 
